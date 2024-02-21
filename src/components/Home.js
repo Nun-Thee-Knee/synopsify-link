0ae54e35e1f05dc15324dbfccecfd6a7c69f3306
NandiniSharma116 <nandinisharma1603@gmail.com>
2024-02-21 19:28:10 +0530

    Added in the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ export const Home = () => {
   return (
     <div className='h-auto bg-black'>
         <div className='flex p-20 bg-black flex-wrap space-x-32 justify-center'>
-            <div className='flex flex-col justify-center items-start text-white  h-[400px] w-[500px]'>
+            <div className='flex flex-col justify-center items-start text-white  h-[400px] w-auto'>
               <h1 className='text-7xl font-extrabold'><span className='text-red-800'>Synopsify</span>Link</h1>
-              <p className='text-xl mt-10'>Transforming Complex Video Narratives into Clear and Actionable Text Summaries: Your Link, Your Knowledge Unleashed.</p>
+              <p className='text-xl mt-10 w-96'>Transforming Complex Video Narratives into Clear and Actionable Text Summaries: Your Link, Your Knowledge Unleashed.</p>
               <button className='hover:bg-red-800 text-xl bg-red-950 mt-10 h-10 w-full rounded-xl'><strong>Go to Workspace</strong></button>
               </div>
             <div className='flex justify-center items-center text-white h-[400px] w-[500px]'>
