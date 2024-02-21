3d06cb31461fea31b8917c1753fa664b490bad64
NandiniSharma116 <nandinisharma1603@gmail.com>
2024-02-21 22:05:30 +0530

    Updated the main page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div className='bg-black h-20 flex justify-center items-center'>
         <ul className='flex space-x-10'>
-          <li><a href="https://icons.getbootstrap.com/icons/discord/"><i class="bi bi-instagram text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></a></li>
-          <li><i class="bi bi-github text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></li>
-          <li><i class="bi bi-linkedin text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></li>
-          <li><i class="bi bi-discord text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></li>
+          <li><a href="https://github.com/NandiniSharma116"><i class="bi bi-github text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></a></li>
+          <li><a href="https://www.instagram.com/nandini_sharma1603/"><i class="bi bi-instagram text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></a></li>
+          <li><a href="https://discord.com/channels/misty0883"><i class="bi bi-discord text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/nandini-sharma-220548290/"><i class="bi bi-linkedin text-zinc-800 hover:text-zinc-500 cursor-pointer"></i></a></li>
         </ul>
     </div>
   )
