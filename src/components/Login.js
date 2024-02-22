4819fcd414117f023255dc167b38c3cf7570d8c9
NandiniSharma116 <nandinisharma1603@gmail.com>
2024-02-22 15:30:37 +0530

    Save the changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,9 +20,9 @@ const Login = (props) => {
         </div>
         <div id='form' className='mt-10 w-full'>
           <form action="">
-            <input className='bg-black rounded-xl text-white w-full border mb-10 mt-10 border-gray-300 rounded' type="email" placeholder="Enter your email" />
-            <input className='bg-black rounded-xl text-white w-full border mb-10 border-gray-300 rounded' type="password" placeholder="Enter your password" />
-            <input className={`${form === "Login"?"hidden":""} bg-black rounded-xl text-white w-full border mb-10 border-gray-300 rounded`} type="password" placeholder="Confirm password" />
+            <input className='bg-black rounded text-white w-full border mb-10 mt-10 border-gray-300' type="email" placeholder="Enter your email" />
+            <input className='bg-black rounded text-white w-full border mb-10 border-gray-300' type="password" placeholder="Enter your password" />
+            <input className={`${form === "Login"?"hidden":""} bg-black text-white w-full border mb-10 border-gray-300 rounded`} type="password" placeholder="Confirm password" />
             <center>
             <button className='hover:bg-red-800 rounded-2xl border-red-800 border-[1px] w-full text-white p-3'>
               <a href="/">{form}</a>
