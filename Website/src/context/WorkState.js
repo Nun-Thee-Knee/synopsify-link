dbb08636757dd02a8aeb5b2b8e41e460169a635a
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2025-04-22 23:14:02 +0530

    Update WorkState.js

--- Website/src/context/WorkState.js
+++ Website/src/context/WorkState.js
@@ -132,6 +132,8 @@ const WorkState = (props) => {
   };
   const changeSummary = async() => {
     setLoading(true);
+    setSummary("");
+    setTitle("");
     try {
       const response = await fetch('http://127.0.0.1:5000', {
         method: 'POST',
